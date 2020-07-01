1227550a6b3b4291955cce00a9b815e0a7df9cc8
Angel-Ho221 <angel.ho@ryerson.ca>
2020-07-01 21:45:42 +0000

    updated

--- client.js
+++ client.js
@@ -1,20 +1,20 @@
 const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '135.23.222.148',
     port: 50541
   });
   // interpret incoming data as text
-  
-  conn.setEncoding('utf8')
+
+  conn.setEncoding('utf8');
   conn.on("connect", connect => {
-    console.log("Successfully connected to game server")
-  })
+    console.log("Successfully connected to game server");
+  });
   conn.on('connect', () => {
-    conn.write("Name: AH")
-  })
-  
+    conn.write("Name: AH");
+  });
+
   // conn.on('connect', () => {
   //   conn.write("Move: up")
   //      setTimeout(() => {
@@ -36,10 +36,9 @@ const connect = function() {
 
   conn.on("data", data => {
     //do smthing when i receive the data
-    console.log(data);   
-  })  
+    console.log(data);
+  });
   return conn;
-
 };
 
-module.exports = {connect};
+module.exports = { connect };
