a093f9cc26e516b18924129c0997f90d725b7b06
Angel-Ho221 <angel.ho@ryerson.ca>
2020-07-01 18:44:52 +0000

    added my name

--- client.js
+++ client.js
@@ -6,7 +6,14 @@ const connect = function() {
     port: 50541
   });
   // interpret incoming data as text
+  
   conn.setEncoding('utf8')
+  conn.on("connect", connect => {
+    console.log("Successfully connected to game server")
+  })
+  conn.on('connect', () => {
+    conn.write("Name: AH")
+  })
   conn.on("data", data => {
     //do smthing when i receive the data
     console.log(data);   
